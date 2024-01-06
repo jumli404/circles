4eb1803d911218b87229e64e895ab528a9115ca5
jumli_d <148221429+jumli404@users.noreply.github.com>
2024-01-06 19:42:42 +0530

    Create scrpit.js

---
+++ scrpit.js
@@ -0,0 +1,29 @@
+ var a = document.getElementById('hm')
+        var b = document.getElementById('h')
+        var c = 0
+        var canvas = document.getElementsByTagName('canvas')[0]
+        canvas.width = innerWidth
+        canvas.height = innerHeight
+        var d = canvas.getContext('2d')
+
+        var x
+        var y;
+        var r = 50
+        function as() {
+            addEventListener('mousemove', (event) => {
+                a.innerHTML = x
+                b.innerHTML = y
+                x = event.x
+                y = event.y
+
+
+            })
+
+            d.beginPath()
+            d.arc(x, y, r, Math.PI * 2, false)
+            d.strokeStyle = 'red'
+            d.stroke()
+            requestAnimationFrame(as)
+        }
+
+        as()
